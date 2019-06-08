7fd6ce3ad1f65e36d21840d52d4bcf225eeb63b4
Rita <rita@dyn-129-236-238-152.dyn.columbia.edu>
2019-06-08 14:23:02 -0400

    Updates

---
+++ precache-manifest.6fc15f5341f596bd7499e01bd7520d5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3826115812bc4d1a1b96985e7462b379",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "975966dec4a5b4fd5755",
+    "url": "/Clicky-Game/static/css/main.9f4d5681.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/Clicky-Game/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "975966dec4a5b4fd5755",
+    "url": "/Clicky-Game/static/js/main.0e882f1f.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  }
+]);
